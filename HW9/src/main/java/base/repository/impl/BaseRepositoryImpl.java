6fd6a18d8d58da158683c44955d314f0d871ef20
Shahrad <shahradbagheri6@gmail.com>
2023-06-20 19:14:37 +0330

    added abstract methods to get implemented when extended

--- HW9/src/main/java/base/repository/impl/BaseRepositoryImpl.java
+++ HW9/src/main/java/base/repository/impl/BaseRepositoryImpl.java
@@ -4,10 +4,12 @@ import base.model.BaseEntity;
 import base.repository.BaseRepository;
 
 import java.io.Serializable;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-public class BaseRepositoryImpl <ID extends Serializable, TYPE extends BaseEntity<ID>> implements BaseRepository {
+public abstract class BaseRepositoryImpl <ID extends Serializable, TYPE extends BaseEntity<ID>> implements BaseRepository {
     @Override
     public void save(BaseEntity entity) throws SQLException {
 
@@ -32,4 +34,13 @@ public class BaseRepositoryImpl <ID extends Serializable, TYPE extends BaseEntit
     public List findAll() throws SQLException {
         return null;
     }
+    public abstract String getTableName();
+
+    public abstract String getColumnsName();
+
+    public abstract String getUpdateQueryParams();
+    public abstract String getCountOfQuestionMarkForParams();
+    public abstract TYPE mapResultSetToEntity(ResultSet resultSet) throws SQLException;
+
+    public abstract void fillParamForStatement(PreparedStatement preparedStatement, TYPE entity) throws SQLException;
 }
