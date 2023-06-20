49ffdb4de18e7603efb7eb4de87ef87df5f9642a
Shahrad <shahradbagheri6@gmail.com>
2023-06-20 21:22:45 +0330

    added Electronics

--- HW9/src/main/java/model/Electronics.java
+++ HW9/src/main/java/model/Electronics.java
@@ -1,46 +1,6 @@
 package model;
 
-import base.model.BaseEntity;
-
-public class Electronics extends BaseEntity<Integer> {
-    private String name;
-    private int stock;
-    private int price;
-
-    public Electronics(String name, int stock, int price) {
-        this.name = name;
-        this.stock = stock;
-        this.price = price;
-    }
-
-    public Electronics(Integer integer, String name, int stock, int price) {
-        super(integer);
-        this.name = name;
-        this.stock = stock;
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
+public enum Electronics {
+    TV,
+    Radio
 }
