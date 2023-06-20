b7a67b2ae3f498c225b734a4758c9b517dcf75e4
Shahrad <shahradbagheri6@gmail.com>
2023-06-20 19:25:26 +0330

    created database connection

--- HW9/src/main/java/config/DatabaseConfig.java
+++ HW9/src/main/java/config/DatabaseConfig.java
@@ -1,5 +1,19 @@
 package config;
 
-public class DatabaseConfig {
+import util.Constant;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    public Connection getCreatedConnection(){
+        try{
+            return DriverManager.getConnection(Constant.URL,Constant.USERNAME,Constant.PASSWORD);
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(e.getErrorCode());
+        }
+        return null;
+    }
 }
