f01d924504c9a7bbbc01029b07e27736f4cafcc3
Shahrad <shahradbagheri6@gmail.com>
2023-06-20 19:26:40 +0330

    added connection to ApplicationContext

--- HW9/src/main/java/util/ApplicationContext.java
+++ HW9/src/main/java/util/ApplicationContext.java
@@ -1,4 +1,13 @@
 package util;
 
+import config.DatabaseConfig;
+
+import java.sql.Connection;
+
 public class ApplicationContext {
+    public static final Connection CONNECTION;
+
+    static {
+        CONNECTION = new DatabaseConfig().getCreatedConnection();
+    }
 }
