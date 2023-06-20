0c93b5a9a09712e8ab48ff6fb10d67d78a04c218
Shahrad <shahradbagheri6@gmail.com>
2023-06-20 19:06:07 +0330

    added field constructor and setter and getters for BaseEntity

--- HW9/src/main/java/base/model/BaseEntity.java
+++ HW9/src/main/java/base/model/BaseEntity.java
@@ -1,4 +1,20 @@
 package base.model;
 
-public class BaseEntity {
+public class BaseEntity <ID>{
+    protected ID id;
+
+    public BaseEntity() {
+    }
+
+    public BaseEntity(ID id) {
+        this.id = id;
+    }
+
+    public ID getId() {
+        return id;
+    }
+    public void setId(ID id){
+        this.id=id;
+
+    }
 }
