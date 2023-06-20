fec9b71c5bc317d176b0dd9862673d57006e2f36
Shahrad <shahradbagheri6@gmail.com>
2023-06-20 19:25:08 +0330

    added constants for dbconnection

--- HW9/src/main/java/util/Constant.java
+++ HW9/src/main/java/util/Constant.java
@@ -1,4 +1,7 @@
 package util;
 
 public class Constant {
+    public static final String URL = "jdbc:postgresql://localhost:5432/hw8";
+    public static final String PASSWORD = "12161213";
+    public static final String USERNAME = "postgres";
 }
