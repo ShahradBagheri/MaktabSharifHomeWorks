cc69b4932af881cd4704a7e253bfddb4a2246f55
Shahrad <shahradbagheri6@gmail.com>
2023-06-21 18:07:30 +0330

    implemented the methods

--- HW9/src/main/java/service/UserService.java
+++ HW9/src/main/java/service/UserService.java
@@ -3,5 +3,16 @@ package service;
 import base.service.BaseService;
 import model.User;
 
+import java.sql.SQLException;
+
 public interface UserService extends BaseService<Integer, User> {
+    boolean isExistsUsername(String username) throws Exception;
+
+    boolean isExistsNatCode(String natCode) throws SQLException;
+
+    boolean isExistsEmail(String phone) throws SQLException;
+
+    boolean isValidEntryData(String regex,String data);
+
+    User checkCredentialInfoForLogin(String username,String password) throws Exception;
 }
