68ce354ebe3a4d444eb691d5a9ea50ea99a9aa59
Shahrad <shahradbagheri6@gmail.com>
2023-06-21 18:44:14 +0330

    added the service and repositories to ApplicationContext

--- HW9/src/main/java/util/ApplicationContext.java
+++ HW9/src/main/java/util/ApplicationContext.java
@@ -1,13 +1,29 @@
 package util;
 
 import config.DatabaseConfig;
+import repository.ShoppingListRepository;
+import repository.UserRepository;
+import repository.impl.ShoppingListRepositoryImpl;
+import repository.impl.UserRepositoryImpl;
+import service.ShoppingListService;
+import service.UserService;
+import service.impl.ShoppingListServiceImpl;
+import service.impl.UserServiceImpl;
 
 import java.sql.Connection;
 
 public class ApplicationContext {
     public static final Connection CONNECTION;
+    public  static final UserRepository USER_REPOSITORY;
+    public static final ShoppingListRepository SHOPPING_LIST_REPOSITORY;
+    public static final UserService USER_SERVICE;
+    public static final ShoppingListService SHOPPING_LIST_SERVICE;
 
     static {
         CONNECTION = new DatabaseConfig().getCreatedConnection();
+        USER_REPOSITORY = new UserRepositoryImpl();
+        SHOPPING_LIST_REPOSITORY = new ShoppingListRepositoryImpl();
+        USER_SERVICE = new UserServiceImpl(USER_REPOSITORY);
+        SHOPPING_LIST_SERVICE = new ShoppingListServiceImpl(SHOPPING_LIST_REPOSITORY);
     }
 }
