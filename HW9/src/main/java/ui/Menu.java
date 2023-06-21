a6cfffa191676a26567b3da564dfae7b46be11c0
Shahrad <shahradbagheri6@gmail.com>
2023-06-21 18:44:31 +0330

    created the first page

--- HW9/src/main/java/ui/Menu.java
+++ HW9/src/main/java/ui/Menu.java
@@ -1,7 +1,27 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Menu {
+    static Scanner scanner = new Scanner(System.in);
     public static void run() {
+        while (true) {
+            System.out.println("Choose an option \n1.Signup\n2.Login\n3.Exit");
+            switch (scanner.nextLine()) {
+                case "1":
+                    signupMenu();
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Not a valid option");
+            }
+        }
+    }
+    public static void signupMenu(){
 
     }
 }
