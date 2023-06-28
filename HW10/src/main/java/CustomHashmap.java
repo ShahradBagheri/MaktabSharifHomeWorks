04112ac64b2f430922f9cb91e45fda31816e5bd6
Shahrad <shahradbagheri6@gmail.com>
2023-06-29 02:24:57 +0330

    added fields and constructors

--- HW10/src/main/java/CustomHashmap.java
+++ HW10/src/main/java/CustomHashmap.java
@@ -1,7 +1,22 @@
 import java.util.List;
+import java.util.Map;
 
 public class CustomHashmap<K, V> {
     private int size;
+    private int capacity;
+    private Map<K,V>[] hashmap;
+
+    public CustomHashmap(){
+        this.size = 0;
+        this.capacity = 16;
+        hashmap = new Map[this.capacity];
+    }
+
+    public CustomHashmap(int capacity){
+        this.size = 0;
+        this.capacity = capacity;
+        hashmap = new Map[capacity];
+    }
 
 
     private static class Map<K, V> {
