a30dd4acd01c857f7869b63ab82a1fde79e60ec5
Shahrad <shahradbagheri6@gmail.com>
2023-06-29 02:14:36 +0330

    created the private map class

--- HW10/src/main/java/CustomHashmap.java
+++ HW10/src/main/java/CustomHashmap.java
@@ -1,3 +1,28 @@
-public class CustomHashmap<K, V> {
+import java.util.List;
 
+public class CustomHashmap<K, V> {
+    private int size;
+
+
+    private static class Map<K, V> {
+        private K key;
+        private V value;
+
+        Map(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        K getKey() {
+            return key;
+        }
+
+        V getValue() {
+            return value;
+        }
+
+        void setValue(V value) {
+            this.value = value;
+        }
+    }
 }
