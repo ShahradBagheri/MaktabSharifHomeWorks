9395c94fed3d29a192533142e7ee5bb1948156e3
Shahrad <shahradbagheri6@gmail.com>
2023-06-29 01:20:40 +0330

    added randomCharTree to create tree

--- HW10/src/main/java/TreeSetQuestion.java
+++ HW10/src/main/java/TreeSetQuestion.java
@@ -1,3 +1,16 @@
+import java.util.Random;
+import java.util.TreeSet;
+
 public class TreeSetQuestion {
 
+    public static TreeSet<Character> randomCharTree(){
+        TreeSet<Character> treeSet = new TreeSet<>();
+        Random random = new Random();
+
+        while (treeSet.size() < 10) {
+            char randomChar = (char) (random.nextInt(26) + 97);
+            treeSet.add(randomChar);
+        }
+        return  treeSet;
+    }
 }
