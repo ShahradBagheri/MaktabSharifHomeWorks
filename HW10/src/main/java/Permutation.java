47493a4c693c60fe71ba90bb70cd62a8a43c6f78
Shahrad <shahradbagheri6@gmail.com>
2023-06-29 00:05:25 +0330

    added generatePermutation added countCharacters to setup the hashmap

--- HW10/src/main/java/Permutation.java
+++ HW10/src/main/java/Permutation.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Permutation {
+    public static List<String> generatePermutations(String word) {
+        List<String> permutations = new ArrayList<>();
+        Map<Character, Integer> charCounts = countCharacters(word);
+        return null;
+    }
+
+    public static Map<Character, Integer> countCharacters(String word) {
+        Map<Character, Integer> charCounts = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            if (charCounts.containsKey(c)) {
+                int count = charCounts.get(c);
+                charCounts.put(c, count + 1);
+            } else {
+                charCounts.put(c, 1);
+            }
+        }
+        return charCounts;
+    }
 }
