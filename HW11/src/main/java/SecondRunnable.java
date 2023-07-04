095cd14716d4540ea2ca560c6009b6dd93b49cc4
Shahrad <shahradbagheri6@gmail.com>
2023-07-04 19:44:39 +0330

    added constructor and fields

--- HW11/src/main/java/SecondRunnable.java
+++ HW11/src/main/java/SecondRunnable.java
@@ -1,4 +1,16 @@
+import java.util.List;
+
 public class SecondRunnable implements Runnable{
+
+    private List<Integer> list;
+    private int listLength;
+
+    public SecondRunnable(List<Integer> list, int listLength) {
+        this.list = list;
+        this.listLength = listLength;
+    }
+
+
     @Override
     public void run() {
 
