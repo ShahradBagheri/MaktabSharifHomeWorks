1c3e3b28235cd67dff1dce9d9d507b19ffac9c04
Shahrad <shahradbagheri6@gmail.com>
2023-07-05 18:14:58 +0330

    added the main to test the program

--- HW11/src/main/java/SecondQuestion/Main.java
+++ HW11/src/main/java/SecondQuestion/Main.java
@@ -1,7 +1,19 @@
 package SecondQuestion;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<String> firstList = new ArrayList<>();
+        List<String> secondList = new ArrayList<>();
 
+        Runnable firstRunnable = new FirstRunnable(firstList,secondList);
+        Runnable secondRunnable = new FirstRunnable(firstList,secondList);
+        Thread thread1 = new Thread(firstRunnable);
+        Thread thread2 = new Thread(secondRunnable);
+
+        thread1.start();
+        thread2.start();
     }
 }
