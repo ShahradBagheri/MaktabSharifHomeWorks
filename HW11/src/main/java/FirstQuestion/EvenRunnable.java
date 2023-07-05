34f3907e0007e13b6131a8cccebb0ba543897750
Shahrad <shahradbagheri6@gmail.com>
2023-07-05 16:05:54 +0330

    fixed name and now it take a list rather than just a number

---
+++ HW11/src/main/java/FirstQuestion/EvenRunnable.java
@@ -0,0 +1,31 @@
+package FirstQuestion;
+
+import java.util.List;
+
+public class EvenRunnable implements Runnable {
+
+    private final List<Integer> sharedList;
+    private final List<Integer> givenList;
+
+    public EvenRunnable(List<Integer> sharedList, List<Integer> givenList) {
+        this.sharedList = sharedList;
+        this.givenList = givenList;
+    }
+
+    @Override
+    public void run() {
+        synchronized (sharedList) {
+            for (Integer evenNumber : givenList) {
+                while (sharedList.size() % 2 == 1) {
+                    try {
+                        sharedList.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                sharedList.add(evenNumber);
+                notify();
+            }
+        }
+    }
+}
