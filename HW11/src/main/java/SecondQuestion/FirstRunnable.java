eb70446d8075b41879503c75322ce31ac2ac714f
Shahrad <shahradbagheri6@gmail.com>
2023-07-05 18:09:51 +0330

    implemented the run method

--- HW11/src/main/java/SecondQuestion/FirstRunnable.java
+++ HW11/src/main/java/SecondQuestion/FirstRunnable.java
@@ -1,8 +1,23 @@
 package SecondQuestion;
 
-public class FirstRunnable implements Runnable{
+import java.util.List;
+
+public class FirstRunnable implements Runnable {
+    List<String> firstList;
+    List<String> secondList;
+
+    public FirstRunnable(List<String> firstList, List<String> secondList) {
+        this.firstList = firstList;
+        this.secondList = secondList;
+    }
+
     @Override
     public void run() {
-
+        synchronized (firstList) {
+            firstList.add("first runnable signature");
+        }
+        synchronized (secondList){
+            secondList.add("second runnable signature");
+        }
     }
 }
