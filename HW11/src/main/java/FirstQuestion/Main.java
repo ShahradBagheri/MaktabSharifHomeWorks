aeba9449e9a247af79e33485cc171762a7645d3a
Shahrad <shahradbagheri6@gmail.com>
2023-07-05 16:16:44 +0330

    fixed the names

--- HW11/src/main/java/FirstQuestion/Main.java
+++ HW11/src/main/java/FirstQuestion/Main.java
@@ -9,13 +9,17 @@ public class Main {
         GenerateList generateList = new GenerateList();
         try{
             int number = scanner.nextInt();
-            Runnable firstRunnable = new EvenRunnable(list,generateList.generateEvenList(number));
-            Runnable secondRunnable = new OddRunnable(list,generateList.generateOddList(number));
-            Thread thread1 = new Thread(firstRunnable);
-            Thread thread2 = new Thread(secondRunnable);
+            Runnable evenRunnable = new EvenRunnable(list,generateList.generateEvenList(number));
+            Runnable oddRunnable = new OddRunnable(list,generateList.generateOddList(number));
+
+            Thread thread1 = new Thread(evenRunnable);
+            Thread thread2 = new Thread(oddRunnable);
 
             thread1.start();
             thread2.start();
+
+            thread2.join();
+
             System.out.println(list);
         }catch (InputMismatchException e){
             System.out.println(e.getMessage());
