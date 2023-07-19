34ada7483bda17d35d3b7c989e5493e7f8694b64
Shahrad <shahradbagheri6@gmail.com>
2023-07-19 20:33:53 +0330

    removed import

--- HW12/src/main/java/stream/Main.java
+++ HW12/src/main/java/stream/Main.java
@@ -1,19 +1,16 @@
 package stream;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Amir","Hataf","Mahdi","Mohammad","Ali","Taher","Reza","Mohsen");
+        List<String> list = Arrays.asList("Amir", "Hataf", "Mahdi", "Mohammad", "Ali", "Taher", "Reza", "Mohsen");
 
-        List<String> sortedList = list.stream().sorted(Comparator.comparing(String::length)).toList();
-        System.out.println(sortedList);
-
-        Map<Integer, List<String>> map = list.stream().collect(Collectors.groupingBy(String::length));
+        Map<Integer, List<String>> map = list.stream()
+                .collect(Collectors.groupingBy(String::length));
         System.out.println(map);
     }
 }
