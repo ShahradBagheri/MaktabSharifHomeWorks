8e23a4957d757604671b4f64757ebefabb592ce0
Shahrad <shahradbagheri6@gmail.com>
2023-07-20 06:01:31 +0330

    removed import

--- HW12/src/main/java/stream/Main.java
+++ HW12/src/main/java/stream/Main.java
@@ -3,14 +3,19 @@ package stream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class Main {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("Amir", "Hataf", "Mahdi", "Mohammad", "Ali", "Taher", "Reza", "Mohsen");
 
-        Map<Integer, List<String>> map = list.stream()
-                .collect(Collectors.groupingBy(String::length));
-        System.out.println(map);
+        Map<Integer, List<String>> map1 = list.stream()
+                .collect(groupingBy(String::length));
+        Map<Integer, Long> map2 = list.stream()
+                .collect(groupingBy(String::length, counting()));
+        System.out.println(map1);
+        System.out.println(map2);
     }
 }
