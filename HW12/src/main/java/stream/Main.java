30b076f58354e22b7e735696ffcfccce7ae0d203
Shahrad <shahradbagheri6@gmail.com>
2023-07-20 06:09:25 +0330

    added mojtaba to the list

--- HW12/src/main/java/stream/Main.java
+++ HW12/src/main/java/stream/Main.java
@@ -9,12 +9,13 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Amir", "Hataf", "Mahdi", "Mohammad", "Ali", "Taher", "Reza", "Mohsen");
+        List<String> list = Arrays.asList("Amir", "Hataf", "Mahdi", "Mojtaba" , "Mohammad", "Ali", "Taher", "Reza", "Mohsen");
 
         Map<Integer, List<String>> map1 = list.stream()
                 .collect(groupingBy(String::length));
         Map<Integer, Long> map2 = list.stream()
                 .collect(groupingBy(String::length, counting()));
+
         System.out.println(map1);
         System.out.println(map2);
     }
