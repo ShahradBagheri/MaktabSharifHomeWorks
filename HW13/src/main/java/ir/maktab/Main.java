5429381c842436c3dd4dd8318b9dd5b881a11c1a
Shahrad <shahradbagheri6@gmail.com>
2023-07-24 06:41:12 +0330

    solved the first and second questions

--- HW13/src/main/java/ir/maktab/Main.java
+++ HW13/src/main/java/ir/maktab/Main.java
@@ -2,9 +2,23 @@ package ir.maktab;
 
 
 import ir.maktab.mockdata.MockData;
+import ir.maktab.model.Person;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MockData.getPeople());
+        System.out.println(firstQuestion(MockData.getPeople()));
+        System.out.println(secondQuestion(MockData.getPeople()));
+    }
+
+    public static List<Person> firstQuestion(List<Person> data) {
+        return data.stream().filter((person -> person.getAge() <= 50)).collect(Collectors.toList());
+    }
+
+    public static List<Person> secondQuestion(List<Person> data){
+        return data.stream().sorted(Comparator.comparing(Person::getUsername)).collect(Collectors.toList());
     }
 }
