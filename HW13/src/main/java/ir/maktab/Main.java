0758a18d879e1dfde7253ac5d25f4e1a0cbff0fd
Shahrad <shahradbagheri6@gmail.com>
2023-07-24 06:09:56 +0330

    started the project

---
+++ HW13/src/main/java/ir/maktab/Main.java
@@ -0,0 +1,10 @@
+package ir.maktab;
+
+
+import ir.maktab.mockdata.MockData;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(MockData.getPeople());
+    }
+}
