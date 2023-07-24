0810fc6833e61e2114beb36d45159858348a2a58
Shahrad <shahradbagheri6@gmail.com>
2023-07-24 06:58:46 +0330

    solved the third and forth questions

--- HW13/src/main/java/ir/maktab/Main.java
+++ HW13/src/main/java/ir/maktab/Main.java
@@ -6,12 +6,15 @@ import ir.maktab.model.Person;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(firstQuestion(MockData.getPeople()));
         System.out.println(secondQuestion(MockData.getPeople()));
+        System.out.println(thirdQuestion(MockData.getPeople()));
+        System.out.println(forthQuestion(MockData.getPeople()));
     }
 
     public static List<Person> firstQuestion(List<Person> data) {
@@ -20,5 +23,13 @@ public class Main {
 
     public static List<Person> secondQuestion(List<Person> data){
         return data.stream().sorted(Comparator.comparing(Person::getUsername)).collect(Collectors.toList());
+    }
+
+    public static List<Person> thirdQuestion(List<Person> data){
+        return data.stream().sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getLastName)).collect(Collectors.toList());
+    }
+
+    public static Set<String> forthQuestion(List<Person> data){
+        return data.stream().map(Person::getIpv4).collect(Collectors.toSet());
     }
 }
