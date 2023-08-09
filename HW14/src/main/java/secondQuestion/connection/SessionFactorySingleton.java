4273c539c65df49c349a79f4d5607ddc8ad8a7a8
Shahrad <shahradbagheri6@gmail.com>
2023-08-09 14:06:41 +0330

    created SessionFactorySingleton

---
+++ HW14/src/main/java/secondQuestion/connection/SessionFactorySingleton.java
@@ -0,0 +1,30 @@
+package secondQuestion.connection;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import secondQuestion.model.Person;
+
+public class SessionFactorySingleton {
+
+    private SessionFactorySingleton() {}
+
+    private static class LazyHolder {
+        static SessionFactory INSTANCE;
+
+        static {
+            var registry = new StandardServiceRegistryBuilder()
+                    .configure()
+                    .build();
+
+            INSTANCE = new MetadataSources(registry)
+                    .addAnnotatedClass(Person.class)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        }
+    }
+
+    public static SessionFactory getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+}
