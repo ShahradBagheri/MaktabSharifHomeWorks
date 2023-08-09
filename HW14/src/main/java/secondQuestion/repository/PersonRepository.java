d0158d46b819e2c7fc99d2ddcda62e785ff69ec6
Shahrad <shahradbagheri6@gmail.com>
2023-08-09 15:07:50 +0330

    added the methods to the PersonRepository

--- HW14/src/main/java/secondQuestion/repository/PersonRepository.java
+++ HW14/src/main/java/secondQuestion/repository/PersonRepository.java
@@ -1,4 +1,17 @@
 package secondQuestion.repository;
 
+import secondQuestion.model.Person;
+
+import java.util.List;
+
 public interface PersonRepository {
+    void save(Person person);
+
+    void update(Person person);
+
+    void delete(Person person);
+
+    List<Person> loadAll();
+
+    boolean contains(Person person);
 }
