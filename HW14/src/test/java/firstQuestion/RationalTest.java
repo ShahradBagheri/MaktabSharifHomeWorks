61eec7ac796f49b8222fb47d58eacb76fef289e7
Shahrad <shahradbagheri6@gmail.com>
2023-08-09 10:58:57 +0330

    added sub and mul tests

--- HW14/src/test/java/firstQuestion/RationalTest.java
+++ HW14/src/test/java/firstQuestion/RationalTest.java
@@ -16,7 +16,17 @@ class RationalTest {
 
     @Test
     public void halfOfOnePlusHalfOfThreeShouldBeTwo(){
-        assertEquals(2,new Rational(1,2).add(new Rational(3,2)).toFloatingPoint());
+        assertEquals(new Rational(2,1) ,new Rational(1,2).add(new Rational(3,2)));
+    }
+
+    @Test
+    public void halfOfTheeSubHalfOfOneShouldBeOne(){
+        assertEquals(new Rational(1,1),new Rational(3,2).sub(new Rational(1,2)));
+    }
+
+    @Test
+    public void halfOfOneMulHalfOfOneShouldBeQuarterOfThree(){
+        assertEquals(new Rational(3,4),new Rational(3,2).mul(new Rational(1,2)));
     }
 
 
