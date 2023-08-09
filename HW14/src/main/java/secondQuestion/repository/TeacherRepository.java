0ec06f18354438c00ecd0caaaaec9b076cf5d3f3
Shahrad <shahradbagheri6@gmail.com>
2023-08-09 23:04:00 +0330

    created and implemented the teacher repository

---
+++ HW14/src/main/java/secondQuestion/repository/TeacherRepository.java
@@ -0,0 +1,19 @@
+package secondQuestion.repository;
+
+import secondQuestion.model.Teacher;
+
+import java.util.List;
+
+public interface TeacherRepository {
+    Teacher save(Teacher teacher);
+
+    void update(Teacher teacher);
+
+    void delete(Teacher teacher);
+
+    Teacher loadById(Long id);
+
+    List<Teacher> loadAll();
+
+    boolean contains(Teacher teacher);
+}
