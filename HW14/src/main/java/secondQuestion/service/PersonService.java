7263b7f18e9287a2eb1dabd951cc8bebe0f34a6b
Shahrad <shahradbagheri6@gmail.com>
2023-08-09 18:43:52 +0330

    implemented the PersonServiceImpl

--- HW14/src/main/java/secondQuestion/service/PersonService.java
+++ HW14/src/main/java/secondQuestion/service/PersonService.java
@@ -1,4 +1,21 @@
 package secondQuestion.service;
 
+import secondQuestion.model.Person;
+
+import java.util.List;
+
 public interface PersonService {
+    Person save(Person person);
+
+    void update(Person person);
+
+    void delete(Person person);
+
+    Person loadById(Long id);
+
+    List<Person> loadAll();
+
+    boolean contains(Person person);
+
+    Person signUp(String firstname,String lastname);
 }
--- HW14/src/main/java/secondQuestion/service/impl/PersonServiceImpl.java
+++ HW14/src/main/java/secondQuestion/service/impl/PersonServiceImpl.java
@@ -1,4 +1,49 @@
 package secondQuestion.service.impl;
 
-public class PersonServiceImpl {
+import secondQuestion.model.Person;
+import secondQuestion.repository.impl.PersonRepositoryImpl;
+import secondQuestion.service.PersonService;
+
+import java.util.List;
+
+public class PersonServiceImpl implements PersonService {
+    private final PersonRepositoryImpl personRepository = new PersonRepositoryImpl();
+
+    @Override
+    public Person save(Person person) {
+        return personRepository.save(person);
+    }
+
+    @Override
+    public void update(Person person) {
+personRepository.update(person);
+    }
+
+    @Override
+    public void delete(Person person) {
+personRepository.delete(person);
+    }
+
+    @Override
+    public Person loadById(Long id) {
+        return personRepository.loadById(id);
+    }
+
+    @Override
+    public List<Person> loadAll() {
+        return personRepository.loadAll();
+    }
+
+    @Override
+    public boolean contains(Person person) {
+        return personRepository.contains(person);
+    }
+
+    @Override
+    public Person signUp(String firstname, String lastname) {
+        Person person = new Person();
+        person.setFirstname(firstname);
+        person.setLastname(lastname);
+        return personRepository.save(person);
+    }
 }
