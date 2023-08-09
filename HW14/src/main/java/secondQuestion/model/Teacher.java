7598a05389f82c93c463b9c0ec12f9c77c5909c6
Shahrad <shahradbagheri6@gmail.com>
2023-08-09 22:24:21 +0330

    added fields to the Teacher class

--- HW14/src/main/java/secondQuestion/model/Teacher.java
+++ HW14/src/main/java/secondQuestion/model/Teacher.java
@@ -1,4 +1,30 @@
 package secondQuestion.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import secondQuestion.enums.TeacherTier;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Teacher extends Person{
+
+    @Column(unique = true)
+    private String teacherId;
+
+    private String major;
+
+    @Enumerated(EnumType.STRING)
+    private TeacherTier teacherTier;
+
+    private double salary;
 }
