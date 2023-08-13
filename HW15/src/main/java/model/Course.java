7b0e059e031a7e8c923fd492d11ffb1422eac33e
Shahrad <shahradbagheri6@gmail.com>
2023-08-13 14:21:55 +0330

    fniished setting up the Professor class

--- HW15/src/main/java/model/Course.java
+++ HW15/src/main/java/model/Course.java
@@ -1,4 +1,26 @@
 package model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class Course {
+
+    @Id
+
+    private Long id;
+
+    @ManyToOne
+    private Student student;
+
+    @ManyToOne
+    private Professor professor;
+
+    private String term;
 }
--- HW15/src/main/java/model/Professor.java
+++ HW15/src/main/java/model/Professor.java
@@ -3,8 +3,7 @@ package model;
 import enumeration.ProfessorTier;
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Set;
 
 @Getter
@@ -16,7 +15,20 @@ import java.util.Set;
 @Entity
 public class Professor extends User{
 
+    @Enumerated(value = EnumType.STRING)
     private ProfessorTier professorTier;
 
+    private Double baseSalary;
+
+    @OneToMany
     private Set<Course> courseSet;
+
+    public double calculateSalary(String  term){
+
+        long courseCount = courseSet.stream().filter(course -> course.getTerm().equals(term)).count();
+
+        if (this.professorTier == ProfessorTier.NORMAL)
+            return courseCount * 1000000;
+        return baseSalary + courseCount * 1000000;
+    }
 }
