18ca5421048abaa58f1ba9d91ab5a14cad39cd6e
Shahrad <shahradbagheri6@gmail.com>
2023-10-04 23:27:08 +0330

    added log commands to TeacherServiceImpl

--- HW17/src/main/java/firstQuestion/service/impl/TeacherServiceImpl.java
+++ HW17/src/main/java/firstQuestion/service/impl/TeacherServiceImpl.java
@@ -5,6 +5,8 @@ import firstQuestion.model.enums.TeacherTier;
 import firstQuestion.repository.impl.TeacherRepositoryImpl;
 import firstQuestion.service.TeacherService;
 import firstQuestion.model.Teacher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
@@ -14,98 +16,126 @@ public class TeacherServiceImpl implements TeacherService {
 
     private final TeacherRepositoryImpl teacherRepository = new TeacherRepositoryImpl();
     private final EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
+    private final Logger logger = LoggerFactory.getLogger(PersonServiceImpl.class);
 
     @Override
     public Teacher save(Teacher teacher) {
+
         EntityTransaction entityTransaction = entityManager.getTransaction();
+        logger.info("attempting to save in {}", PersonServiceImpl.class.getSimpleName());
+
         try {
             entityTransaction.begin();
 
             teacher = teacherRepository.save(teacher);
 
             entityTransaction.commit();
+            logger.info("saved successfully in {}", PersonServiceImpl.class.getSimpleName());
             return teacher;
         } catch (Exception e) {
             entityTransaction.rollback();
+            logger.error("failed to save in {}", PersonServiceImpl.class.getSimpleName());
             return null;
         }
     }
 
     @Override
     public void update(Teacher teacher) {
+
         EntityTransaction entityTransaction = entityManager.getTransaction();
+        logger.info("attempting to update in {}", PersonServiceImpl.class.getSimpleName());
         try {
             entityTransaction.begin();
 
             teacherRepository.update(teacher);
 
             entityTransaction.commit();
+            logger.info("updated successfully in {}", PersonServiceImpl.class.getSimpleName());
         } catch (Exception e) {
             entityTransaction.rollback();
+            logger.error("failed to update in {}", PersonServiceImpl.class.getSimpleName());
         }
     }
 
     @Override
     public void delete(Teacher teacher) {
+
         EntityTransaction entityTransaction = entityManager.getTransaction();
+        logger.info("attempting to delete in {}", PersonServiceImpl.class.getSimpleName());
+
         try {
             entityTransaction.begin();
 
             teacherRepository.delete(teacher);
 
             entityTransaction.commit();
+            logger.info("deleted successfully in {}", PersonServiceImpl.class.getSimpleName());
         } catch (Exception e) {
             entityTransaction.rollback();
+            logger.error("failed to delete in {}", PersonServiceImpl.class.getSimpleName());
         }
     }
 
     @Override
     public Teacher loadById(Long id) {
+
         EntityTransaction entityTransaction = entityManager.getTransaction();
+        logger.info("attempting to load by id in {}", PersonServiceImpl.class.getSimpleName());
         try {
             entityTransaction.begin();
 
             Teacher teacher = teacherRepository.loadById(id);
 
             entityTransaction.commit();
+            logger.info("loaded by id successfully in {}", PersonServiceImpl.class.getSimpleName());
 
             return teacher;
         } catch (Exception e) {
             entityTransaction.rollback();
+            logger.error("failed to load by id in {}", PersonServiceImpl.class.getSimpleName());
             return null;
         }
     }
 
     @Override
     public List<Teacher> loadAll() {
+
         EntityTransaction entityTransaction = entityManager.getTransaction();
+        logger.info("attempting to loadAll by id in {}", PersonServiceImpl.class.getSimpleName());
         try {
             entityTransaction.begin();
 
             List<Teacher> teachers = teacherRepository.loadAll();
 
             entityTransaction.commit();
+            logger.info("loadedAll successfully in {}", PersonServiceImpl.class.getSimpleName());
 
             return teachers;
         } catch (Exception e) {
             entityTransaction.rollback();
+            logger.error("failed to loadAll in {}", PersonServiceImpl.class.getSimpleName());
             return null;
         }
     }
 
     @Override
     public boolean contains(Teacher teacher) {
+
         EntityTransaction entityTransaction = entityManager.getTransaction();
+        logger.info("attempting to check contains in {}", PersonServiceImpl.class.getSimpleName());
+
         try {
             entityTransaction.begin();
 
             boolean contains = teacherRepository.contains(teacher);
 
             entityTransaction.commit();
+            logger.info("checked contains successfully in {}", PersonServiceImpl.class.getSimpleName());
 
             return contains;
         } catch (Exception e) {
             entityTransaction.rollback();
+            logger.error("failed to check contains in {}", PersonServiceImpl.class.getSimpleName());
             return false;
         }
     }
@@ -118,6 +148,7 @@ public class TeacherServiceImpl implements TeacherService {
         teacher.setTeacherId(teacherId);
         teacher.setTeacherTier(teacherTier);
 
+        logger.info("attempting to signUp in {}", PersonServiceImpl.class.getSimpleName());
         return teacherRepository.save(teacher);
     }
 }
