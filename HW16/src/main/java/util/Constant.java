9a042bb971e792a1e0c70fe3b2ff15185d76e95f
Shahrad <shahradbagheri6@gmail.com>
2023-10-04 04:03:26 +0330

    changed date

--- HW16/src/main/java/util/Constant.java
+++ HW16/src/main/java/util/Constant.java
@@ -5,6 +5,6 @@ import java.util.List;
 
 public class Constant {
 
-    public static final LocalDate DATE = LocalDate.of(2024,8,2);
+    public static final LocalDate DATE = LocalDate.of(2029,8,2);
     public static final List<String> BIG_CITIES = List.of("gilan","isfahan","azar bayejan sharghi","fars","khozestan","ghom","khorasan razavo","alborz");
 }
