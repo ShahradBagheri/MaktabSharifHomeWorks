100c2d0bfa68cc8b69377f7ad230b78dc550a468
Shahrad <shahradbagheri6@gmail.com>
2023-10-04 04:39:39 +0330

    adapting to JPA

--- HW17/src/main/java/firstQuestion/repository/impl/PersonRepositoryImpl.java
+++ HW17/src/main/java/firstQuestion/repository/impl/PersonRepositoryImpl.java
@@ -1,77 +1,58 @@
 package firstQuestion.repository.impl;
 
+import firstQuestion.connection.EntityManagerSingleton;
 import firstQuestion.connection.SessionFactorySingleton;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import firstQuestion.model.Person;
 import firstQuestion.repository.PersonRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 public class PersonRepositoryImpl implements PersonRepository {
 
-    private final SessionFactory sessionFactory = SessionFactorySingleton.getInstance();
+    private final EntityManager entityManager = EntityManagerSingleton.getInstanceEM();
 
     @Override
     public Person save(Person person) {
-        var session = sessionFactory.getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-        Long id = (Long) session.save(person);
-        Person savedPerson = session.get(Person.class,id);
-        transaction.commit();
-        return savedPerson;
-    }
-
-    @Override
-    public void update(Person person) {
-        var session = sessionFactory.getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-        session.update(person);
-        transaction.commit();
-    }
-
-    @Override
-    public void delete(Person person) {
-        var session = sessionFactory.getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(person);
-        transaction.commit();
-    }
-
-    @Override
-    public Person loadById(Long id) {
-        var session = sessionFactory.getCurrentSession();
-        Transaction transaction = session.beginTransaction();
-        Person person = session.get(Person.class,id);
-        transaction.commit();
+        entityManager.persist(person);
         return person;
     }
 
     @Override
+    public void update(Person person) {
+        entityManager.merge(person);
+    }
+
+    @Override
+    public void delete(Person person) {
+        entityManager.remove(person);
+    }
+
+    @Override
+    public Person loadById(Long id) {
+        return entityManager.find(Person.class,id);
+    }
+
+    @Override
     public List<Person> loadAll() {
-        var session = sessionFactory.getCurrentSession();
-        Transaction transaction = session.beginTransaction();
         String hql = "FROM Person";
-        Query<Person> query = session.createQuery(hql,Person.class);
-        List<Person> result = query.list();
-        transaction.commit();
-        return result;
+        TypedQuery<Person> query = entityManager.createQuery(hql,Person.class);
+        return query.getResultList();
     }
 
     @Override
     public boolean contains(Person person) {
-        var session = sessionFactory.getCurrentSession();
-        Transaction transaction = session.beginTransaction();
         String hql = "SELECT COUNT(*) FROM Person p WHERE p.id = :personId AND p.firstname = :personFirstname AND p.lastname = :personLastname AND p.birthdate = :personBirthdate";
-        Query<Long> query = session.createQuery(hql,Long.class);
+        TypedQuery<Long> query = entityManager.createQuery(hql,Long.class);
         query.setParameter("personFirstname",person.getFirstname());
         query.setParameter("personLastname",person.getLastname());
         query.setParameter("personId",person.getId());
         query.setParameter("personBirthdate",person.getBirthdate());
 
         Long result = query.getSingleResult();
-        transaction.commit();
         return result != 0;
     }
 }
